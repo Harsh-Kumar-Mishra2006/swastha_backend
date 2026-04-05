fa675258ec226c30f7c23bdc6a42bab6c1bba4b0
Harsh-Kumar-Mishra <mishrakumarharsh987@gmail.com>
2026-04-05 22:09:42 +0530

    fixed

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const doctorRoutes = require('./routes/doctorRoutes');
 const appointmentRoutes = require('./routes/appointmentRoutes');
 const paymentRoutes = require('./routes/paymentRoutes');
 const publicRoutes = require('./routes/publicRoutes');
-const testReportRoutes = require('./routes/testReportRoute');
+const testReportRoutes = require('./routes/testReportRoutes');
 
 const app = express();
 connectDB();
